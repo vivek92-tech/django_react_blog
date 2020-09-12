886823b87692411a38eef59b31a7e7447ac39296
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-09-11 23:38:35 -0400

    creating the blogDetails.js frontend and connection it with our API

--- frontend/src/Components/blogDetails.js
+++ frontend/src/Components/blogDetails.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 
-const blogDetail = () => (
-    <div>
-        blogDetail
+
+const BlogDetail = (props) => {
+    const [blog, setBlog] = useState({});
+
+    useEffect(()=> {
+        const slug = props.match.params.id;
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/${slug}`);
+                setBlog(response.data);
+            }
+            catch (error){
+
+            }
+        };
+        fetchData();
+    }, [props.match.params.id]);
+
+     const createBlog =() => {
+        return {__html: blog.content}
+    };
+
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        else
+            return '';
+    };
+
+
+return (
+    <div className='container mt-3'>
+        <h1 className='display-2'>{blog.title}</h1>
+        <h2 className='text-muted mt-3'>Category: {capitalizeFirstLetter(blog.category)}</h2>
+        <h4>{blog.month} {blog.day}</h4>
+        <div className='mt-5 mb-5 ' dangerouslySetInnerHTML={createBlog()} />
+        <hr />
+        <p className='lead mb-5'><Link to='/blog' className='front-weight-bold'>Back to Blogs</Link></p>
     </div>
 );
-
-export default blogDetail;
+};
+export default BlogDetail;
