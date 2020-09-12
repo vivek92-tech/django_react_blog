7f2d433022cbe045011a6660c4a6d62dbc6ca33d
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-09-11 22:43:40 -0400

    creating the blog.js frontend and connection it with our API

--- frontend/src/Components/home.js
+++ frontend/src/Components/home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const home = () => (
-    <div>
-        home
+    <div className= 'container'>
+        <div className="jumbotron jumbotron-fluid mt-5">
+            <div className="container">
+                <h3 className="display-4">Welcome to Django-React-Blog!</h3>
+                <i className="lead">Blogging is to writing what extreme sports are to athletics: more free-form, more accident-prone, less formal, more alive. It is, in many ways, writing out loud.</i>
+                <hr class="my-4" />
+                    
+                        <Link className="btn btn-primary btn-lg" to='/blog' role="button">Check out the blog</Link>
+                    
+            </div>
+        </div>
     </div>
 );
 
