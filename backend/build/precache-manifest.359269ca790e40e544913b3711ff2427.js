be708098c00a4a82df6c13607747966737178e27
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-09-12 00:19:52 -0400

    Adding the updated build to the backend

---
+++ backend/build/precache-manifest.359269ca790e40e544913b3711ff2427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607a49e4c23a478ef1cd193e4ea981b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b429f61d743eec1615a",
+    "url": "/static/js/2.ed258354.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed258354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02203f30110f5f7f2499",
+    "url": "/static/js/main.b5713b50.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
