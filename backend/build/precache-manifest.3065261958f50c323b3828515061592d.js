a7f2fd3b5039c5118357e37a13b0be257998a2bc
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-09-17 23:02:42 -0400

    Addint the final build

---
+++ backend/build/precache-manifest.3065261958f50c323b3828515061592d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ebe400a79548872ecb97e26216ae48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fad189167ab04cfc9f4",
+    "url": "/static/css/main.069ea914.chunk.css"
+  },
+  {
+    "revision": "8e2b94b4151e4cb23e55",
+    "url": "/static/js/2.f83deac2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f83deac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fad189167ab04cfc9f4",
+    "url": "/static/js/main.b7b32d5d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "338dc17cd51c630a30a7e31343fb22e4",
+    "url": "/static/media/2.338dc17c.jpg"
+  },
+  {
+    "revision": "d19d7a83d65bae030eb50e00bfbf569e",
+    "url": "/static/media/google-rainbow-texture-1491566442.d19d7a83.jpg"
+  }
+]);
